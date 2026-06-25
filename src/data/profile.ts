2fb8f8e9e75ab1eb667ff5a5fa484022a5be373a
Dayalan D S <dayalands2210@gmail.com>
2026-06-26 02:21:22 +0530

    content corrections

--- src/data/profile.ts
+++ src/data/profile.ts
@@ -5,15 +5,15 @@ export const profileData = {
     "Full Stack Engineer with 3 years of experience across distributed systems, microservices, and cloud-native applications.",
   greeting: "HELLO WORLD! ",
   bio: [
-    "Full Stack Engineer with 3 years delivering secure applications across microservices and distributed systems, from design to production support.",
-    "Specialized in building production-grade microservices using Java, Spring, and React/Angular with MVC and Event-Driven Architecture, while ensuring robust security and compliance with enterprise standards",
-    "Passionate about developing scalable, AI-powered products and resilient backend architectures. With hands-on experience building complex data lakehouses and financial instruments, I am highly motivated to drive technical innovation across domains like E-Commerce, Banking, Finance, Payments, Data & AI.",
+    "Full Stack Engineer at Ford GTBC Chennai, with 3 years of delivering secure applications across microservices and distributed systems, from design to production support.",
+    "Specialized in building production-grade microservices using Java, Spring, and React/Angular in MVC and Event-Driven Architecture, while ensuring robust security and compliance with enterprise standards.",
+    "Passionate about advancing into large‑scale, real‑time applications across domains such as E‑Commerce, Banking, Finance, Payments, and Data & AI, while building deep expertise in AI/ML integration and Big Data technologies.",
   ],
   stats: [
-    { label: "Years of Experience", value: "Three" }, //[cite: 1]
-    { label: "Core Tech", value: "Java, Spring, React/Angular, GCP" }, //[cite: 1]
-    { label: "Domains", value: " Logistics, Fintech, E-Commerce" }, //[cite: 1]
-    { label: "Data & AI", value: "Pub/Sub, BigQuery, Spring AI" }, //[cite: 1]
+    { label: "Years of Experience", value: "Three" }, 
+    { label: "Core Tech", value: "Java, Spring, React/Angular, GCP" }, 
+    { label: "Domains", value: " Logistics, Catalog, E-Commerce" }, 
+    { label: "Data & AI", value: "Pub/Sub, BigQuery, Spring AI" }, 
   ],
   resumeUrl: "#",
   avatarUrl: "https://raw.githubusercontent.com/daya1an/portfolio/main/assets/profile-picture.png",
